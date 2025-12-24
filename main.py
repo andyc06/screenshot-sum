e106bdfcc42a3744759a501045db170a23384c70
Andy Collier <andrew@andrewben.com>
2025-12-24 15:40:47 -0800

    use osascript for dialog box

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import time
 import pytesseract
 import pandas
+import os
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 from PIL import Image
@@ -38,10 +39,25 @@ class Event(FileSystemEventHandler):
     def on_moved(self, event):
         new_screenshot = event.dest_path
         numbers_series = get_image_numbers(new_screenshot)
+        numbers_series_string = numbers_series.to_string(index=False)
+        total_sum = numbers_series.sum()
+        combined_message = """
+        New screenshot found: {new_screenshot}
+        Numbers detected:
+        {numbers_series_string}
+        Sum: {total_sum}
+        """.format(new_screenshot=new_screenshot, numbers_series_string=numbers_series_string, total_sum=total_sum)
         print("New screenshot created: ", new_screenshot)
         print("Numbers detected:")
-        print(numbers_series.to_string(index=False))
+        print(numbers_series_string)
         print("Sum: ", numbers_series.sum())
+
+        os.system(
+            'osascript -e \'tell application "System Events" to display dialog "{}" with title "Message"\''.format(
+                combined_message.replace('"', '\\"')
+            )
+        )
+
 
 
 # watch the screenshot directory
